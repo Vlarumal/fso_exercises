0ba324e062d8ef307451dd71738ceedccf54cde4
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-15 09:37:29 +0200

    complete: 2.2

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,6 +1,10 @@
 const Total = ({ parts }) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises {total}</p>;
+  return (
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
+  );
 };
 
 export default Total;
