b0de4c622e1f7255e2f72c2c5177ebf088bfd05c
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-15 09:34:45 +0200

    complete: 2.1

---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,19 @@
+import Part from "./Part";
+import Total from "./Total";
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part
+          key={`${part.id}`}
+          partName={part.name}
+          exercises={part.exercises}
+        />
+      ))}
+      <Total parts={parts} />
+    </>
+  );
+};
+
+export default Content;
