b5d2f4594979733fd38e844214b82910b210d22b
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-15 14:21:15 +0200

    complete:: 2.9*

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [filteredPersons, setFilteredPersons] = useState([]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (e) => {
     e.preventDefault();
     const person = {
+      id: persons.length + 1,
       name: newName,
       number: newNumber,
     };
@@ -19,6 +25,7 @@ const App = () => {
       alert(`${nameToCheck} is already added to the phonebook`);
       return;
     }
+
     setPersons(persons.concat(person));
     setNewName("");
     setNewNumber("");
@@ -32,6 +39,14 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleSearchChange = (e) => {
+    const personToSearchFor = e.target.value.toLowerCase();
+    const filteredPersons = persons.filter((person) =>
+      person.name.toLocaleLowerCase().includes(personToSearchFor)
+    );
+    setFilteredPersons(filteredPersons);
+  };
+
   const checkForDuplicates = (name) => {
     return persons.some((person) => person.name === name);
   };
@@ -39,9 +54,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        filter shown with
+        <input
+          type='text'
+          onChange={handleSearchChange}
+        />
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name:{" "}
+          name:
           <input
             value={newName}
             onChange={handleNameChange}
@@ -50,7 +73,7 @@ const App = () => {
           />
         </div>
         <div>
-          number:{" "}
+          number:
           <input
             type='tel'
             value={newNumber}
@@ -63,13 +86,17 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <>
-        {persons.map((person) => (
-          <p key={person.name}>
-            {person.name} {person.number}
-          </p>
-        ))}
-      </>
+      {filteredPersons.length > 0
+        ? filteredPersons.map((person) => (
+            <p key={person.id}>
+              {person.name} {person.number}
+            </p>
+          ))
+        : persons.map((person) => (
+            <p key={person.id}>
+              {person.name} {person.number}
+            </p>
+          ))}
     </div>
   );
 };
