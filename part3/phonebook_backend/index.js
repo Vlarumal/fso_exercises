94bf31a3e70669410921294cddc9a1a265971ed9
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-16 20:19:48 +0200

    complete: 3.8*

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,7 +3,16 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("post-data", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-data"
+  )
+);
 
 let persons = [
   {
