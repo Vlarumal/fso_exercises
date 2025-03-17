31ecea0af666b428ff78bb337a3b9301712a438c
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-17 16:56:45 +0200

    complete: 3.15

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -58,10 +58,9 @@ app.post("/api/persons", (req, res) => {
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  res.status(204).end();
+  Person.findByIdAndDelete(req.params.id).then((result) => {
+    res.status(204).end();
+  });
 });
 
 app.get("/info", (req, res) => {
