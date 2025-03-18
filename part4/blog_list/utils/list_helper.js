117d71aa1571093a619bf39e7f00549d5b15fe0f
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-18 17:56:07 +0200

    complete: 4.5*

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
     : blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes)
+  const mostLikes = Math.max(...likes)
+  const favorite = blogs.find((blog) => blog.likes === mostLikes)
+
+  return favorite
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
