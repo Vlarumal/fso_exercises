644fb4caba3329a86b4645437812eb364685db74
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-20 20:32:41 +0200

    complete: 4.18

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -2,9 +2,10 @@ const { MONGODB_URI } = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = MONGODB_URI
 mongoose
@@ -21,6 +22,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
