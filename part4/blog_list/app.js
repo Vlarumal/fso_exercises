cd54202fb7734635c6f3eaa62dcbb3089dc3c1bd
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-20 17:35:19 +0200

    complete: 4.15

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 const mongoUrl = MONGODB_URI
@@ -19,5 +20,25 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (
+    error.name === 'MongoServerError' &&
+    error.message.includes('E11000 duplicate key error')
+  ) {
+    return response
+      .status(400)
+      .json({ error: 'expected `username` to be unique' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 module.exports = app
