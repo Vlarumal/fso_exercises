717a75cc8144697658dd777056904884a4f92155
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-31 20:51:14 +0300

    complete: 6.16-6.17

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,26 +1,7 @@
 import ReactDOM from 'react-dom/client'
-
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
+import store from './store'
 import App from './App'
-import anecdoteReducer, {
-  setAnecdotes,
-} from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
-import notificationReducer from './reducers/notificationReducer'
-import anecdoteService from './services/anecdotes'
-
-const store = configureStore({
-  reducer: {
-    anecdotes: anecdoteReducer,
-    filter: filterReducer,
-    notification: notificationReducer,
-  },
-})
-
-anecdoteService
-  .getAll()
-  .then((anecdotes) => store.dispatch(setAnecdotes(anecdotes)))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
-    createAnecdote(state, action) {
-      state.push(action.payload)
-    },
     likeAnecdote(state, action) {
       const id = action.payload.id
       const anecdoteToChange = state.find(
@@ -29,10 +27,20 @@ const anecdoteSlice = createSlice({
   },
 })
 
-export const {
-  createAnecdote,
-  likeAnecdote,
-  appendAnecdote,
-  setAnecdotes,
-} = anecdoteSlice.actions
+export const { likeAnecdote, appendAnecdote, setAnecdotes } =
+  anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
+  }
+}
 export default anecdoteSlice.reducer
