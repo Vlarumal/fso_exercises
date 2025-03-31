14bee2daee180e493083e00807b0b24203581d49
Vlarumal <177475520+Vlarumal@users.noreply.github.com>
2025-03-31 19:10:29 +0300

    complete: 6.14

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
+import anecdoteReducer, {
+  setAnecdotes,
+} from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
+import anecdoteService from './services/anecdotes'
 
 const store = configureStore({
   reducer: {
@@ -14,6 +17,10 @@ const store = configureStore({
     notification: notificationReducer,
   },
 })
+
+anecdoteService
+  .getAll()
+  .then((anecdotes) => store.dispatch(setAnecdotes(anecdotes)))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
